2f6b5af881d76d1035d00f3e1b83e92ec9c4c009
Niclas Wuestenbecker <niclaswue@gmail.com>
2020-03-20 23:53:34 +0100

    pyaudio requirements

--- start.py
+++ start.py
@@ -1,4 +1,15 @@
 import sys
+import speech_recognition as sr
 print(sys.version)
-print("Hello World!")
-print("TEST")
+print(sr.__version__)
+r = sr.Recognizer()
+sr.Microphone.list_microphone_names()
+
+#mic = sr.Microphone(device_index=3)
+
+# with mic as source:
+
+#with harvard as source:
+#    audio = r.record(source)
+
+# r.recognize_google(audio)
